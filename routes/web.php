74128e17ac1f012caeacb7148b218043aee1b89b
Raphael Vagapov <rr03342@gmail.com>
2024-09-05 15:40:49 +0500

    master fix route

--- routes/web.php
+++ routes/web.php
@@ -18,26 +18,6 @@ use Inertia\Inertia;
 |
 */
 
-Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
-});
-//Route::get('/', function () {
-//    return view('welcome');
-//});
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-Route::get('/messages', [TweetsController::class, 'vue'])->name('messages');
+Route::get('/', [TweetsController::class, 'vue'])->name('messages');
 Route::post('/messagesSend', [TweetSendController::class, 'Send'])->name('SendMessage');
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-
 require __DIR__.'/auth.php';
